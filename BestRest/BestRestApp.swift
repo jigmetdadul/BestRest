1a315bb35555116c5d585404a50e633bf159595c
Jigmet Dadul <85403349+jigmetdadul@users.noreply.github.com>
2024-01-24 19:11:30 +0530

    Initial Commit

---
+++ BestRest/BestRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BestRestApp.swift
+//  BestRest
+//
+//  Created by Jigmet stanzin Dadul on 24/01/24.
+//
+
+import SwiftUI
+
+@main
+struct BestRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
