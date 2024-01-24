7cc7afc2c46480be84c4d44ffe1cc932f971d600
Jigmet Dadul <85403349+jigmetdadul@users.noreply.github.com>
2024-01-24 20:01:34 +0530

    adding ML Model

--- BestRest/ContentView.swift
+++ BestRest/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var wakeUp = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            
+            Form {
+                Section("Enter the details") {
+                    VStack {
+                        Stepper("Desired Amount of Sleep", value: $sleepAmount, in: 1...14, step: 0.25)
+                        Spacer(minLength: 10)
+                        Text("\(sleepAmount)").bold()
+                    }
+                    VStack {
+                        Stepper("Coffee Intake", value: $coffeeAmount, in: 1...8)
+                        Spacer(minLength: 10)
+                        Text("\(coffeeAmount)").bold()
+                    }
+                }
+                Button("My rest time") {
+                    sleepAmount = 234
+                }
+                .padding(10)
+                .background(Color.cyan)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+            }
+            .navigationTitle("Better Rest")
+            .toolbar {
+            
+                Button("Reset", action: calculateRestTime)
+                    .buttonStyle(.bordered)
+                    .tint(.orange)
+            }
+            
         }
-        .padding()
+    }
+    
+    
+    func calculateRestTime(){
+        
     }
 }
 
