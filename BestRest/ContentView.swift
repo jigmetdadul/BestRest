c8d9704e1a23971c7b3e62aa590fbc52aee6de9f
Jigmet Dadul <85403349+jigmetdadul@users.noreply.github.com>
2024-01-25 19:42:50 +0530

    implemented calculate functino

--- BestRest/ContentView.swift
+++ BestRest/ContentView.swift
@@ -6,12 +6,16 @@
 //
 
 import SwiftUI
+import CoreML
+
 
 struct ContentView: View {
-    @State private var wakeUp = Date.now
+    @State private var wakeUpTime = Date.now
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
-    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
     
     var body: some View {
         NavigationView {
@@ -20,7 +24,7 @@ struct ContentView: View {
                 Section("Enter the details") {
                     HStack{
                         Text("Your desired wake up time   ")
-                        DatePicker("", selection: $wakeUp, displayedComponents: .hourAndMinute).labelsHidden()
+                        DatePicker("", selection: $wakeUpTime, displayedComponents: .hourAndMinute).labelsHidden()
                     }
                     VStack {
                         Stepper("Desired Amount of Sleep", value: $sleepAmount, in: 1...14, step: 0.25)
@@ -33,8 +37,8 @@ struct ContentView: View {
                         Text("\(coffeeAmount)").bold()
                     }
                 }
-                Button("My rest time") {
-                    //
+                Button("Get bed Time"){
+                    calculateRestTime()
                 }
                 .padding(10)
                 .background(Color.cyan)
@@ -48,13 +52,34 @@ struct ContentView: View {
                     .buttonStyle(.bordered)
                     .tint(.orange)
             }
+            .alert(alertTitle, isPresented: $showingAlert) {
+                Button("Re Calculate") {
+                    
+                }
+            } message: {
+                Text(alertMessage)
+            }
             
         }
     }
     
     
-    func calculateRestTime(){
-        
+    func calculateRestTime()->Void{
+        do{
+            let config = MLModelConfiguration()
+            let SleepModel = try SleepCalculator(configuration: config)
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUpTime)
+            let hours = (components.hour ?? 0) * 60 * 60
+            let minutes = (components.minute ?? 0) * 60
+            let prediction = try SleepModel.prediction(coffee: Int64(coffeeAmount), estimatedSleep: sleepAmount, wake: Int64(hours + minutes))
+            let sleepTime = wakeUpTime - prediction.actualSleep
+            alertTitle = "Your bed Time"
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+        }catch{
+            alertTitle = "Failure"
+            alertMessage = "Sorry, request failed"
+        }
+        showingAlert = true
     }
 }
 
